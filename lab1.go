68760845464255d000090dd42389b858d28cd4e8
Sergius98 <bigsergey1998@gmail.com>
2018-09-19 23:52:03 +0300

    created "func getInfo" and UaToMorse file

--- lab1.go
+++ lab1.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+func code() {
 
+}
+func decode() {
+
+}
+func getInfo(st1,st2 string) string {
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Print("input "+st1+"[default "+st2+"]:")
+	language,_:=reader.ReadString('\n')
+	if lng:=len(language); lng < 2 {
+		return st2
+	} else {
+		return language[:lng-1]
+	}
+}
 func main() {
-
-	fmt.Println("Get started!")
+	language := getInfo("language","ua")
+	fmt.Println(language)
+	fileName := getInfo("FileName","text")
+	fmt.Println(fileName)
+	resultName := getInfo("Result FileName","output")
+	fmt.Println(resultName)
+	direction := getInfo("direction(either 'code' or 'decode')","decode")
+	fmt.Println(direction)
 }
