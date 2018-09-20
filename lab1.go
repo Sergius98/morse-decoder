9b79ee5cdf797eb026dc8565bd15b811de371438
Sergius98 <bigsergey1998@gmail.com>
2018-09-21 00:47:37 +0300

    finished program

--- lab1.go
+++ lab1.go
@@ -5,12 +5,35 @@ import (
 	"os"
 	"bufio"
 	"io/ioutil"
+	"strings"
 )
-func code() {
-
+func code(arr []rune, dict map[string]string) string {
+	str := ""
+	for i:=0;i<len(arr);i++{
+		if smb:=dict[string(arr[i])];smb!=""{
+			str += smb + " "
+		}
+	}
+	return str
 }
-func decode() {
-
+func getCode(arr []rune, i int)(int, string){
+	word := []rune("")
+	for pos:=i; pos<len(arr); pos++{
+		if arr[pos]==32{
+			return pos+1, string(word)
+		} else {
+			word = append(word, arr[pos])
+		}
+	}
+	return len(arr), string(word)
+}
+func decode(arr []rune, dict map[string]string) string {
+	str, code := "",""
+	for i:=0;i<len(arr);{
+		i,code = getCode(arr,i)
+		str += dict[code]
+	}
+	return str
 }
 func getInfo(st1,st2 string) string {
 	var reader = bufio.NewReader(os.Stdin)
@@ -30,7 +53,7 @@ func check(e error) {
 func getData(file string)string{
 	dat, err := ioutil.ReadFile(file)
 	check(err)
-	return string(dat)
+	return strings.ToLower(string(dat))
 }
 func getRunes(file string)[]rune{
 	str := getData(file)
@@ -48,6 +71,9 @@ func getWord(smb,str string, i int)(int, string){
 	return len(str), word
 }
 func updateDictionary(code,key string, reverse bool, dict map[string]string) map[string]string{
+	if (code[0:1]==" ")&&(key==""){
+		code,key = code[1:]," "
+	}
 	if reverse {
 		dict[code] = key
 	} else {
@@ -68,14 +94,19 @@ func getDict(file string, reverse bool)map[string]string{
 	return dict
 }
 func root(language string, fileName string, resultName string, reverse bool) {
-	arr := getRunes(fileName)
-	fmt.Println(arr)
-	dict := getDict(language, reverse)
-	fmt.Println(dict)
+	arr, dict, str := getRunes(fileName), getDict(language, reverse), " "
+	if reverse {
+		str = decode(arr, dict)
+	} else {
+		str = code(arr, dict)
+	}
+	err := ioutil.WriteFile(resultName, []byte(str), 0644)// Unix permission bits
+	check(err)
+	fmt.Println("program successfully finished")
 }
 func getVars() (string, string, string, string) {
 	language := getInfo("language","ua")
-	fileName := getInfo("FileName","part1")
+	fileName := getInfo("FileName","input")
 	resultName := getInfo("Result FileName","output")
 	direction := getInfo("direction(either 'code' or 'decode')","decode")
 	return language, fileName, resultName, direction
