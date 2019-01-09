ee0a7dc6ba64d9b1ec4ea8df1d418e76fccb1565
Sergius98 <bigsergey1998@gmail.com>
2019-01-10 00:36:51 +0200

    finished testing

--- filegener/gener.go
+++ filegener/gener.go
@@ -14,25 +14,33 @@ func check(e error) {
 func main() {
 	dat, err := ioutil.ReadFile("input")
 	check(err)
+	dat12, err := ioutil.ReadFile("input_very_short")
+	check(err)
 
 	f, err := os.Create("input_long")
   check(err)
 	defer f.Close()
-	for i:=0; i<19; i++ {
+	for i:=0; i<4; i++ {
 		f.Write(dat)
 	}
+	f.Write(dat12)
+	f.Write(dat12)
 	f.Sync()
 	fmt.Println("input successfully finished")
 
-	dat2, err2 := ioutil.ReadFile("output")
-	check(err2)
+	dat2, err := ioutil.ReadFile("output")
+	check(err)
+	dat22, err := ioutil.ReadFile("output_very_short")
+	check(err)
 
-	f2, err2 := os.Create("output_long")
-  check(err2)
+	f2, err := os.Create("output_long")
+  check(err)
 	defer f2.Close()
-	for i:=0; i<19; i++ {
+	for i:=0; i<4; i++ {
 		f2.Write(dat2)
 	}
+	f2.Write(dat22)
+	f2.Write(dat22)
 	f2.Sync()
 	fmt.Println("output successfully finished")
 
